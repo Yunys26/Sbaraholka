b47202bd177e68750c27712b5485a56159a875e4
avoreshin <avoreshin@gmail.com>
2023-03-25 21:25:09 +0300

    TEAM42-11-add MUI, Header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import {Link, Outlet} from "react-router-dom";
 import {URLs} from "../../__data__/constants/url";
-import {Button} from "@salutejs/plasma-ui";
+import Button from "@mui/material/Button"
+import './Header.scss'
+// @ts-ignore
+import logo from '../../assets/svg/Logo.svg'
+import LoginIcon from '@mui/icons-material/Login';
+import {SearchComponent} from "./SearchComponent/SearchComponent";
 
 export const Header = () => {
+
     return (
         <>
+            <div className="header__wrapper">
+                <div className="header__logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div>
+                    <SearchComponent/>
+                    <Button><LoginIcon/></Button>
+                </div>
+            </div>
             <div>
-                <Button><Link to={URLs.main}> Home</Link></Button>
+                <><Link to={URLs.main}> Home</Link></>
                 <Link to={URLs.auth}> Auth</Link>
                 <Link to={URLs.user}> User Profile</Link>
             </div>
---
+++ src/widgets/Header/SearchComponent/SearchComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Input, InputAdornment} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
+export const SearchComponent = (props) => {
+    return (
+        <>
+            <Input
+                placeholder={"поиск"}
+                sx={{m: 1, width: '470px'}}
+                startAdornment={
+                    <InputAdornment position="start">
+                        <SearchIcon/>
+                    </InputAdornment>}
+            ></Input>
+        </>
+
+    );
+}
