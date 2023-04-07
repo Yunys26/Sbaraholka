405d208a96ada92c5fb49f90842fa78854295304
AngelinaCP <89658366303@mail.ru>
2023-04-07 19:57:57 +0300

    feat: stubs added

--- stubs/api/index.js
+++ stubs/api/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
 
-module.exports = router;
+const productsList = require('../templates/mock.json')
+
+module.exports = router
+    .get('/catalogue', (req, res) => {
+        res.send(productsList)
+    })
