99970e6e65a7158274215d3983276a79e7ba93d0
AngelinaCP <89658366303@mail.ru>
2023-04-07 19:58:03 +0300

    feat: stubs added

--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -2,11 +2,11 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react"
 
 export const productsAPI = createApi({
     reducerPath: 'productsAPI',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://angelinacp.github.io'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:8099/api'}),
     endpoints: (build) => ({
         fetchAllProducts: build.query({
             query: () => ({
-                url: 'mock.json',
+                url: '/catalogue',
             })
         })
     })
