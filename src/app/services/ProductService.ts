dd40494251cb8caa4cb561f78150ad3eb7b8c398
AngelinaCP <89658366303@mail.ru>
2023-04-08 09:13:33 +0300

    feat: stubs added

--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -2,11 +2,11 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react"
 
 export const productsAPI = createApi({
     reducerPath: 'productsAPI',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:8099/api'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:8099'}),
     endpoints: (build) => ({
         fetchAllProducts: build.query({
             query: () => ({
-                url: '/catalogue',
+                url: '/api',
             })
         })
     })
--- stubs/api/index.js
+++ stubs/api/index.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const productsList = require('../templates/mock.json')
 
 module.exports = router
-    .get('/catalogue', (req, res) => {
+    .get('/', (req, res) => {
         res.send(productsList)
     })
