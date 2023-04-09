28cc1c1c29be18925790ad55df1b237afea22999
AngelinaCP <89658366303@mail.ru>
2023-04-09 23:27:34 +0300

    feat: auth added

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,13 +1,56 @@
 import React from 'react';
-import {useFetchAllProductsQuery} from "../../app/services/ProductService";
+import { Container, TextField, Box, Typography } from '@mui/material';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import { useGetAuthQuery } from '../../app/services/Service';
+import { useNavigate } from 'react-router-dom';
 
 export const AuthPage = () => {
-    
-    const {data} = useFetchAllProductsQuery()
+    const { data } = useGetAuthQuery();
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+    };
 
     return (
-        <div>
-            AuthPage
-        </div>
+        <Container maxWidth='sm'>
+            <Box
+                sx={{
+                    width: 400,
+                    maxWidth: '100%',
+                    margin: '0 auto',
+                    marginTop: '10rem',
+                }}
+            >
+                <Typography variant='h4'>Вход</Typography>
+                <div>
+                    <TextField
+                        id='outlined-basic'
+                        variant='outlined'
+                        margin='dense'
+                        fullWidth
+                        helperText='Логин'
+                        onChange={(e) => setLogin(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id='outlined-basic'
+                        variant='outlined'
+                        margin='dense'
+                        fullWidth
+                        helperText='Пароль'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <Button variant='contained' size='large' onClick={handleClick}>
+                        Войти
+                    </Button>
+                </div>
+            </Box>
+        </Container>
     );
 };
