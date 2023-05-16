0d3cb0e86a41c4a880964a1e6772712c11b278f5
avoreshin <avoreshin@gmail.com>
2023-05-16 16:36:43 +0300

    TEAM42-4 Add MainPage Cards

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import {useFetchAllProductsQuery} from "../../app/services/ProductService";
+import {CardItem, ICardItem} from "../../widgets/CardItem/Carditem";
+import {Box, Paper, Stack} from "@mui/material";
 
 export const MainPage = () => {
+
+    // @ts-ignore
+    const {data}: ICardItem =  useFetchAllProductsQuery()
+    console.log(data)
     return (
         <>
+            <Stack direction="row">
+                <Paper elevation={3}>
+                    {/*Профиль {data?.user_id}*/}
+                    {/* {data?.user_name}*/}
+                </Paper>
+
+            </Stack>
+
             <h1>Main</h1>
+            <Stack>
+                {data?.map((item, index) => {
+                    return (
+                        <CardItem key={index} {...item}/>
+                    )
+                })}
+            </Stack>
+
         </>
     )
 }
